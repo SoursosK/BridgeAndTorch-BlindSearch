4b4bdd5771c1e998826fc38db626bfe675fc305f
Konstantinos Soursos <kon.soursos_@hotmail.com>
2019-04-27 18:06:58 +0300

    creating the structure of the Node class

--- BridgeAndTorch.java
+++ BridgeAndTorch.java
@@ -1,42 +1,76 @@
+
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.Scanner;
-//bfs dfs ucs ids
+import java.util.ArrayList;
+
+//javac BridgeAndTorch.java && java BridgeAndTorch
+
 public class BridgeAndTorch {
 
-    private int state[];
-    private int costs[];
+    private int finiteState[] = { 1, 1, 1, 1, 1 };
+    private int state[] = { 0, 0, 0, 0, 0 };
+    private int costs[] = { 1, 2, 5, 10 };
+
+    public BridgeAndTorch(int state[], int costs[]) {
+
+        // this.finiteState = state;
+        // Arrays.fill(this.finiteState, 1);
+        // this.state = state;
+        // this.costs = costs;
+
+        // System.out.println("The finiteState are:");
+        // for (int i = 0; i < finiteState.length; i++) {
+        // System.out.println(finiteState[i]);
+        // }
+        
+        BFS();
+    }
+
+    private void BFS() {
+
+        LinkedList<Node> queue = new LinkedList<>();
+        queue.add(new Node(this, null, this.state, 0, 0));
 
 
-    public BridgeAndTorch(int state[], int costs[]){
-        this.state = state;
-        this.costs = costs;
+    }
 
+
+    
+
+    public int getCrossingTime(int i){
+        return this.costs[i];
+    }
+
+    public int calculateCrossingTime(int i, int j) {
+        if(this.costs[i] > this.costs[j])
+            return this.costs[i];
+        else    
+            return this.costs[j];
     }
 
     public static void main(String[] args) {
-        int state[];
-        int costs[];
+        // int state[];
+        // int costs[];
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Please insert the number of people waiting to cross the bridge:");
-    
-        int numberOfPeople = sc.nextInt();
-        state = new int[numberOfPeople+1];
-        costs = new int[numberOfPeople];
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Please insert the number of people waiting to cross the
+        // bridge:");
 
-        for(int i=0; i<costs.length; i++){
-            System.out.println("Please insert the crossing time of Person " + Integer.sum(i, 1) + ":");
-            costs[i] = sc.nextInt();
-        }
+        // int numberOfPeople = sc.nextInt();
+        // state = new int[numberOfPeople + 1];
+        // costs = new int[numberOfPeople];
 
-        sc.close();
-        BridgeAndTorch bridgeAndTorch = new BridgeAndTorch(state, costs);
-
-        // System.out.println("The costs are:");
-        // for(int i=0; i<cost.length; i++){
-        //     System.out.println(cost[i]);
+        // for (int i = 0; i < costs.length; i++) {
+        // System.out.println("Please insert the crossing time of Person " +
+        // Integer.sum(i, 1) + ":");
+        // costs[i] = sc.nextInt();
         // }
 
-    }
+        // sc.close();
+        // BridgeAndTorch bridgeAndTorch = new BridgeAndTorch(state, costs);
 
+        BridgeAndTorch bridgeAndTorch = new BridgeAndTorch(null, null);
+    }
 
 }
--- Node.java
+++ Node.java
@@ -1,11 +1,60 @@
+import java.util.ArrayList;
 
+import javax.swing.text.AsyncBoxView.ChildState;
 
 public class Node {
+    private BridgeAndTorch bt;
 
-    int state[];
+    private Node parentNode;
+    private ArrayList<Node> childs;
+    private int state[];
 
-    public Node() {
+    private int depth;
+    private int cost;
 
+    public Node(BridgeAndTorch bt, Node parentNode, int state[], int depth, int cost) {
+        this.bt = bt;
 
+        this.parentNode = parentNode;
+        this.state = state;
+
+        this.depth = depth;
+        this.cost = cost;
+
+        createChilds();
     }
+
+    private void createChilds() {
+
+        // if the torch is on the left side
+        if (state[state.length - 1] == 0)
+            for (int i = 0; i < state.length - 1; i++)
+                for (int j = i + 1; j < state.length - 1; j++)
+                    if (i == 0 && j == 0) {
+                        int chileState[] = this.state;
+                        childState[i] = 1;
+                        childState[j] = 1;
+                        childState[state.length - 1] = 1; //torch goes right
+
+                        int crossingTime = bt.calculateCrossingTime(i, j);
+
+                        childs.add(new Node(bt, this, childState, this.depth + 1, this.cost + crossingTime));
+                    }
+
+        // if the torch is on the right side
+        if (state[state.length - 1] == 1)
+            for (int i = 0; i < state.length - 1; i++)
+                if (i == 1) {
+                    int chileState[] = this.state;
+                    childState[i] = 0;
+                    childState[state.length - 1] = 0; //torch goes left
+
+                    int crossingTime = bt.getCrossingTime(i);
+
+                    childs.add(new Node(bt, this, childState, this.depth + 1, this.cost + crossingTime));
+                }
+    }
+
+
+
 }
