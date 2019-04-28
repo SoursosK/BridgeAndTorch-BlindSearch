f6e1e0ed0e5e852b398473020a6cc9502fef9d7d
Konstantinos Soursos <kon.soursos_@hotmail.com>
2019-04-28 23:23:06 +0300

    Reduced the complexity for the program output

--- BridgeAndTorch.java
+++ BridgeAndTorch.java
@@ -8,11 +8,24 @@ import java.util.ArrayList;
 
 public class BridgeAndTorch {
 
+    private LinkedList<Node> dataStructure;
+
     private int finiteState[] = { 1, 1, 1, 1, 1 };
     private int state[] = { 0, 0, 0, 0, 0 };
     private int costs[] = { 1, 2, 5, 10 };
 
+    private int createdNodesCounter;
+    private int visitedNodesCounter;
+
+    // private int finiteState[] = { 1, 1, 1, 1 };
+    // private int state[] = { 0, 0, 0, 0 };
+    // private int costs[] = { 1, 2, 5 };
+
     public BridgeAndTorch(int state[], int costs[]) {
+
+        dataStructure = new LinkedList<>();
+        this.createdNodesCounter = 0;
+        this.visitedNodesCounter = 0;
 
         // this.finiteState = state;
         // Arrays.fill(this.finiteState, 1);
@@ -23,30 +36,84 @@ public class BridgeAndTorch {
         // for (int i = 0; i < finiteState.length; i++) {
         // System.out.println(finiteState[i]);
         // }
-        
+
         BFS();
     }
 
     private void BFS() {
+        dataStructure.add(new Node("startingNode", this, null, this.state, 0, 0));
+        this.createdNodesCounter++;
 
-        LinkedList<Node> queue = new LinkedList<>();
-        queue.add(new Node(this, null, this.state, 0, 0));
+        while (!dataStructure.isEmpty()) {
+            this.visitedNodesCounter++;
 
+            System.out.println(this.visitedNodesCounter);
 
+            Node node = dataStructure.getFirst();
+            System.out.println("\nnode " + node.getName() + " depth: " + node.getDepth());
+            dataStructure.removeFirst();
+
+            if (!node.checkFiniteState()) {
+                node.createChilds();
+            } else {
+                System.out.println("\nBFS found a solution to the problem!");
+                System.out.println("The final cost is " + node.getCost() + " minutes.");
+                System.out.println("The nodes created were " + this.createdNodesCounter + ", with "
+                        + this.visitedNodesCounter + " of them being visited.\n");
+
+                ArrayList<String> output = new ArrayList<>();
+
+                while (node.getDepth() != 0) {
+                    if (node.getName().length() > 1) {
+                        String s = "A" + node.getName().charAt(0) + " and A" + node.getName().charAt(1) + " crossed the bridge in " 
+                            + ( node.getCost() - node.getParentNode().getCost() ) + " minute(s)";
+                        output.add(s);
+                    } else {
+                        String s = "A" + node.getName() + " returns back in " + ( node.getCost() - node.getParentNode().getCost() ) + " minute(s)";
+                        output.add(s);
+                    }
+                    node = node.getParentNode();
+                }
+
+                for(int i=output.size() - 1; i >= 0; i--) {
+                    System.out.println(output.get(i));
+                }
+
+                // print out the results
+                break;
+            }
+
+            //testing purposes
+            // try {
+            // Thread.sleep(1300);
+            // } catch (Exception e) {
+            // System.out.println("exception caught");
+            // }
+
+        }
     }
 
+    public void insertNode(Node node) {
+        this.dataStructure.add(node);
+    }
 
-    
-
-    public int getCrossingTime(int i){
+    public int getCrossingTime(int i) {
         return this.costs[i];
     }
 
     public int calculateCrossingTime(int i, int j) {
-        if(this.costs[i] > this.costs[j])
+        if (this.costs[i] > this.costs[j])
             return this.costs[i];
-        else    
+        else
             return this.costs[j];
+    }
+
+    public int[] getFiniteState() {
+        return this.finiteState;
+    }
+
+    public void incrementCreatedNodesCounter() {
+        this.createdNodesCounter++;
     }
 
     public static void main(String[] args) {
--- Node.java
+++ Node.java
@@ -1,8 +1,9 @@
 import java.util.ArrayList;
-
-import javax.swing.text.AsyncBoxView.ChildState;
+import java.util.Arrays;
 
 public class Node {
+    private String name;
+
     private BridgeAndTorch bt;
 
     private Node parentNode;
@@ -12,49 +13,121 @@ public class Node {
     private int depth;
     private int cost;
 
-    public Node(BridgeAndTorch bt, Node parentNode, int state[], int depth, int cost) {
+    public Node(String name, BridgeAndTorch bt, Node parentNode, int state[], int depth, int cost) {
+        this.name = name;
+        
         this.bt = bt;
 
         this.parentNode = parentNode;
+        this.childs = null;
         this.state = state;
 
         this.depth = depth;
         this.cost = cost;
-
-        createChilds();
     }
 
-    private void createChilds() {
+    public boolean checkFiniteState(){
+        if(Arrays.equals(this.state, this.bt.getFiniteState()))
+            return true;
+        else
+            return false;
+    }
+
+    private void insertChildsToDataStructure() {
+        for (Node childNode : childs){
+            bt.incrementCreatedNodesCounter();
+            this.bt.insertNode(childNode);
+        }
+    }
+
+    public void createChilds() {
+        this.childs = new ArrayList<>();
 
         // if the torch is on the left side
         if (state[state.length - 1] == 0)
+            
             for (int i = 0; i < state.length - 1; i++)
-                for (int j = i + 1; j < state.length - 1; j++)
-                    if (i == 0 && j == 0) {
-                        int chileState[] = this.state;
-                        childState[i] = 1;
-                        childState[j] = 1;
-                        childState[state.length - 1] = 1; //torch goes right
+                for (int j = i + 1; j < state.length - 1; j++){
+
+                    if (this.state[i] == 0 && this.state[j] == 0) {
+                        int childatate[] = this.state.clone();
+                        childatate[i] = 1;
+                        childatate[j] = 1;
+                        childatate[childatate.length - 1] = 1; // torch goes right
 
                         int crossingTime = bt.calculateCrossingTime(i, j);
 
-                        childs.add(new Node(bt, this, childState, this.depth + 1, this.cost + crossingTime));
+                        childs.add(new Node(i+""+j, bt, this, childatate, this.depth + 1, this.cost + crossingTime));
+                        
+                        System.out.println("child created left side");
                     }
+                }
 
         // if the torch is on the right side
         if (state[state.length - 1] == 1)
+
             for (int i = 0; i < state.length - 1; i++)
-                if (i == 1) {
-                    int chileState[] = this.state;
+
+                if (this.state[i] == 1) {
+                    int childState[] = this.state.clone();
                     childState[i] = 0;
-                    childState[state.length - 1] = 0; //torch goes left
+                    childState[state.length - 1] = 0; // torch goes left
 
                     int crossingTime = bt.getCrossingTime(i);
 
-                    childs.add(new Node(bt, this, childState, this.depth + 1, this.cost + crossingTime));
+                    childs.add(new Node(i+"", bt, this, childState, this.depth + 1, this.cost + crossingTime));
+                    
+                    System.out.println("child created right side");
                 }
+
+        insertChildsToDataStructure();
     }
 
+    
+
+    public Node getParentNode() {
+        return parentNode;
+    }
+
+    public void setParentNode(Node parentNode) {
+        this.parentNode = parentNode;
+    }
+
+    public ArrayList<Node> getChilds() {
+        return childs;
+    }
+
+    public void setChilds(ArrayList<Node> childs) {
+        this.childs = childs;
+    }
+
+    public int[] getState() {
+        return state;
+    }
+
+    public void setState(int[] state) {
+        this.state = state;
+    }
+
+    public int getDepth() {
+        return depth;
+    }
+
+    public void setDepth(int depth) {
+        this.depth = depth;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    public String getName() {
+        return name;
+    }
 
 
 }
